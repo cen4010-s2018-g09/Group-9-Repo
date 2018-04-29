7fc9e2a5489cd5b29689ce629017324514903a9a
a-mitchell818 <35646619+a-mitchell818@users.noreply.github.com>
2018-04-29 16:34:09 -0400

    Add files via upload

--- inventory.php
+++ inventory.php
@@ -1,11 +1,5 @@
 <?php
 session_start();
-$page = strrchr($_SERVER['REQUEST_URI'],'/') ;
-$page = substr($page,1,-4);
-$pType = $_SESSION["profile_type"];
-
-echo "<script src='js/redirect.js'></script>";
-echo "<script>userCheck('".$pType."','".$page."');</script>";
 
       $servername = "localhost";
       if(isset($_SESSION["accNumber"])){
@@ -77,14 +71,9 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-          
-          <li class="nav-item" id = "kit-cart">
-                
-                </li>
-                
+           
             <li class="nav-item">
-                    <span id="cart-bubble" class="notify-bubble"></span>
-                    <a href="shoppingcart.php"><img  src="images/SC2.png" width="30px" height="30px" alt=""></a>
+                    <img src="images/SC2.png" width="30px" height="30px" alt="">
             </li>
             <li class="nav-item">
               <a class="nav-link" href="signin.php">Log Out</a>
@@ -95,32 +84,57 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
       </div>
     </nav>   
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top fixed-top-2">
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto" id="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-            </li>
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top fixed-top-2">
             
-          </ul>
-          <div class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" id="search-box" placeholder="Search" onkeypress="searchBar(this.value);" aria-label="Search">
-          </div>
-        </div>
-      </nav>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                
+                <li class="nav-item active">
+                  <a class="nav-link" href="inventory.php">Inventory </a>
+                </li>
+
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Job Requests </a>
+                </li>
+
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Orders </a>
+                </li>
+
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Vendors </a>
+                </li>
+
+                <li class="nav-item active">
+                        <a class="nav-link" href="#">Profiles </a>
+                </li>
+                
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
     <!-- Page Content -->
     <div class="home">
     <div >
         <div class="row">
         <div class="col-2">
                 <div class="nav flex-column nav-pills d-none d-sm-block" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-               </div>
+                        <a class="nav-link sideBar"  href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Add New Profile</a>
+                        <a class="nav-link sideBar" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Request New Item</a>
+                        <a class="nav-link sideBar" href="newItem.php" role="tab" aria-controls="v-pills-messages" aria-selected="false">Add New Item</a>
+                        <a class="nav-link sideBar" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Create New Kit</a>
+                        <a class="nav-link sideBar" href="#v-pills-vendor" role="tab" aria-controls="v-pills-vendor" aria-selected="false">Add New Vendor</a>
+                    </div>
                       
         </div>
         <div class="col" style="margin-right:0.5rem;">
@@ -142,17 +156,12 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
 
     <!-- Custom JavaScript -->
     <script src="js/itemDetails.js"></script>
-    <script src="js/homePage.js"></script>
-    <script src="js/ShoppingCartItems.js"></script>
-    <script src="js/searchBar.js"></script>
   </body>
 
   <script>
     <?php
       $servername = "localhost";
       $userID = $_SESSION["accNumber"];
-      $profile_type = $_SESSION["profile_type"];
-      echo 'usersetting('."'".$profile_type."'".');';
       try{
     
         $username= "CEN4010_S2018g09";
@@ -161,25 +170,11 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
         PDO("mysql:host=$servername;dbname=CEN4010_S2018g09",trim($username),trim($password));
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         
-        //Connect to Inventory Table
         $stmt = $conn->prepare("SELECT * FROM Inventory "); 
         $stmt->execute();
         $flag=$stmt->setFetchMode(PDO::FETCH_ASSOC);
         $result = $stmt->fetchALL();
-
-        //Connect to ShoppingCart Table
-        $stmt2 = $conn->prepare("SELECT * FROM ShoppingCart WHERE Account_Number = '$userID';"); 
-        $stmt2->execute();
-        $flag2=$stmt2->setFetchMode(PDO::FETCH_ASSOC);
-        $result2 = $stmt2->fetchALL();
-        $quantity=0;
-        for($i=0;$i<count($result2);$i++)
-        {
-            $row2 = $result2[$i];
-            $quantity += $row2['Quantity'];
-          }
-        echo "updateBubble(".$quantity.");";
-
+        
         if(count($result)>0)
         {
             
@@ -192,25 +187,15 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
                 
                 $part_number =$row['Part Number'];
                 $part_name =$row['Part Name'];
-                $part_name = str_replace('"','',$part_name);
                 $vendor =$row['Vendor P/N'];
                 $price =$row['Price'];
                 $quantity =$row['Quantity'];
                 $description =$row['Description'];
-                $keywords =$row['Keyword1'].', '.$row['Keyword2'] ;
-                $description = str_replace('"','',$description);
                 
-                if ($pType == "Student"){
-
-                echo "displayItems('".$part_number."','".$part_name."','".$vendor."','".$price."','".$quantity."','".$description."','".$keywords."');";
-                
-                }
-                else{
-               echo "displayItemsA('".$part_number."','".$part_name."','".$vendor."','".$price."','".$quantity."','".$description."','".$keywords."');";
-                }
-              }
+                echo 'displayItems("'.$part_number.'","'.$part_name.'","'.$vendor.'","'.$price.'","'.$quantity.'","'.$description.'");';
+            }
         }
-        
+       
         
         }
 
@@ -219,18 +204,17 @@ echo "<script>userCheck('".$pType."','".$page."');</script>";
     catch(PDOException $e){
       echo "Connection failed: " . $e->getMessage();
   }
-  
-    
+  if(isset($_POST['view'])){
+    $_SESSION["part_name"] = $_COOKIE["name"];
+    $_SESSION["part_number"] = $_COOKIE["number"];
+    $_SESSION["vendor"] = $_COOKIE["vendor"];
+    $_SESSION["price"] = $_COOKIE["price"];
+    $_SESSION["quantity"] = $_COOKIE["quantity"];
+    $_SESSION["description"] = $_COOKIE["description"];
+    echo 'window.location = "itemDetails.php";';
+  }     
      
     ?>
-    
   </script>
 
 </html>
-
-
-
-
-
-
-
