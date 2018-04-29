cac3a08fa99f2a79f34738d10e0642118b653bdf
a-mitchell818 <35646619+a-mitchell818@users.noreply.github.com>
2018-04-29 16:35:45 -0400

    Add files via upload

--- confirm.php
+++ confirm.php
@@ -4,7 +4,7 @@ session_start();
       $servername = "localhost";
       if(isset($_SESSION["accNumber"])){
       $userID = $_SESSION["accNumber"];
-
+      
     }else
     {
         $link = "signin.php";
@@ -21,7 +21,7 @@ session_start();
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Inventory</title>
+    <title>Item Details</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -37,17 +37,6 @@ session_start();
           padding-top: 56px;
         }
       }
-      * {
-    box-sizing: border-box;
-      }
-
-          h1{ 
-          color: white;
-          }
-          
-          p{ 
-          color: white;
-          }
 
     </style>
 
@@ -55,47 +44,66 @@ session_start();
 
   <body>
 
-   <!-- Navigation -->
-   <nav class="navbar navbar-expand fixed-top logoNav">
-      <div class="container">
-          <img src="images/HNLogo5.png" alt="">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-          <li class="nav-item" id = "kit-cart">
-                
-                </li>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand fixed-top logoNav">
+  <div class="container">
+      <img src="images/HNLogo5.png" alt="">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+       
+        <li class="nav-item">
+                <span id="cart-bubble" class="notify-bubble">0</span>
+                <img src="images/SC2.png" width="30px" height="30px" alt="">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="signin.php">Log Out</a>
+        </li>
 
-            <li class="nav-item">
-                    <span id="cart-bubble" class="notify-bubble">0</span>
-                    <a href="shoppingcart.php"><img  src="images/SC2.png" width="30px" height="30px" alt=""></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="signin.php">Log Out</a>
-            </li>
+      </ul>
+    </div>
+  </div>
+</nav>   
 
-          </ul>
-        </div>
-      </div>
-    </nav>   
-
-   <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top fixed-top-2">
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top fixed-top-2">
         
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto" id="navbar-nav">
+          <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
               <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
             </li>
+            
+            <li class="nav-item active">
+              <a class="nav-link" href="inventory.php">Inventory</a>
+            </li>
 
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Job Requests</a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Orders</a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Vendors</a>
+            </li>
+
+            <li class="nav-item active">
+                    <a class="nav-link" href="#">Profiles</a>
+            </li>
             
           </ul>
-
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </div>
       </nav>
     <!-- Page Content -->
@@ -103,16 +111,28 @@ session_start();
     <div >
         <div class="row">
         <div class="col-2">
-                <div class="nav flex-column nav-pills d-none d-sm-block" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                </div>
-                      
+            <div class="nav flex-column nav-pills d-none d-sm-block" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link sideBar"  href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Add New Profile</a>
+                <a class="nav-link sideBar" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Request New Item</a>
+                <a class="nav-link sideBar" href="newItem.php" role="tab" aria-controls="v-pills-messages" aria-selected="false">Add New Item</a>
+                <a class="nav-link sideBar" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Create New Kit</a>
+                <a class="nav-link sideBar" href="#v-pills-vendor" role="tab" aria-controls="v-pills-vendor" aria-selected="false">Add New Vendor</a>
+            </div>
         </div>
-        <div class="col" style="margin-right:0.5rem;">
-        <div class="mybox">
-          <h1>Thank you for you order</h1>
-          <p>Details of your order pick location or status of you order or request will be emailed within 24-48 hours</p>
-        </div>
+        <div id="item-details-content" class="col-7">
+            <h1 id="product-name"></h1>
+            <div class="row">
+            <div id="product-image" class="col-6"></div>
+            <div id="product-detail" class="col-6"></div>
+            </div>
+           
+            <h3>Product Description</h3>
+            <div id="product-description">
 
+            </div>
+                
+                
+        
         </div>
             
             
@@ -121,27 +141,18 @@ session_start();
             </div>
       </div>
 </div>
-      
-      
-    
-     
-      
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Custom JavaScript -->
-    <!--<script src="js/itemDetails.js"></script>-->
-    <script src="js/homePage.js"></script>
-    <script src="js/ShoppingCartItems.js"></script>
+    <script src="js/jobrequest.js"></script>
+   
   </body>
-
   <script>
-    <?php
+<?php
       $servername = "localhost";
       $userID = $_SESSION["accNumber"];
-      $profile_type = $_SESSION["profile_type"];
-      echo 'usersetting('."'".$profile_type."'".');';
+      $part_number =$_SESSION["part_number"];
+      
       try{
     
         $username= "CEN4010_S2018g09";
@@ -149,61 +160,47 @@ session_start();
         $conn = new
         PDO("mysql:host=$servername;dbname=CEN4010_S2018g09",trim($username),trim($password));
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        echo 'console.log('."'".$part_number."'".');';
         
-        //Connect to Inventory Table
         $stmt = $conn->prepare("SELECT * FROM Inventory "); 
         $stmt->execute();
         $flag=$stmt->setFetchMode(PDO::FETCH_ASSOC);
         $result = $stmt->fetchALL();
-
-        //Connect to ShoppingCart Table
-        $stmt2 = $conn->prepare("SELECT * FROM ShoppingCart WHERE Account_Number = '$userID';"); 
-        $stmt2->execute();
-        $flag2=$stmt2->setFetchMode(PDO::FETCH_ASSOC);
-        $result2 = $stmt2->fetchALL();
-        $quantity=0;
-        for($i=0;$i<count($result2);$i++)
+        
+        if(count($result)>0)
         {
-            $row2 = $result2[$i];
-            $quantity += $row2['Quantity'];
-          }
-        echo "updateBubble(".$quantity.");";
-
-        //if(count($result)>0)
-        //{
             
             
-           // for($i=0;$i<count($result);$i++)
-            //{
+            for($i=0;$i<count($result);$i++)
+            {
+                
+    
+                $row = $result[$i];
                 
                 
-               // $row = $result[$i];
+                if($row["job_type"] == $job_type){
+                $_SESSION["file_one"] = $row['File 1'];
+                $_SESSION["file_two"] = $row['File 2'];
+                $_SESSION["comments"] = $row['Comments'];
+              }
                 
-                //$part_number =$row['Part Number'];
-                //$part_name =$row['Part Name'];
-                //$vendor =$row['Vendor P/N'];
-                //$price =$row['Price'];
-                //$quantity =$row['Quantity'];
-                //$description =$row['Description'];
-                
-                //echo 'displayItems("'.$part_number.'","'.$part_name.'","'.$vendor.'","'.$price.'","'.$quantity.'","'.$description.'");';
-                
-             // }
-       // }
-        
-        
+            }
         }
 
     
-    
+    }
     catch(PDOException $e){
       echo "Connection failed: " . $e->getMessage();
   }
-  
+      
     
-     
-    ?>
-    
-  </script>
+  $job_type =$_SESSION["job_type"];
+  $file_one =$_SESSION["file_one"];
+  $file_two =$_SESSION["file_two"];
+  $comments =$_SESSION["comments"]; 
 
+
+      echo 'displayJobDetails("'.$job_type.'","'.$file_one.'","'.$file_two.'","'.$comments.'");';
+    ?>
+    </script>
 </html>
